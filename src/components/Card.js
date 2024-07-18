7a19443fe0bd1779e748d2af683f9100e18c121b
Daniel Somade <flexsidee@gmail.com>
2024-07-19 00:49:04 +0100

    removed the images completely but zipped them

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,37 @@
-import { createStyles, Paper, Text, rem } from "@mantine/core";
+import { createStyles, Paper, Text, rem, Skeleton } from "@mantine/core";
+import { useEffect, useState } from "react";
 import LazyLoad from "react-lazyload";
 
 const Card = ({ imageUrl, category }) => {
 	const { classes } = useStyles();
+	const [imageLoaded, setImageLoaded] = useState(false);
+	const [imageError, setImageError] = useState(false);
+
+	useEffect(() => {
+		const img = new Image();
+		img.src = imageUrl;
+		img.onload = () => setImageLoaded(true);
+		img.onerror = () => setImageError(true);
+	}, [imageUrl]);
 
 	return (
-		<LazyLoad offset={100}>
-			<Paper
-				shadow="md"
-				p="xl"
-				radius="md"
-				sx={{ backgroundImage: `url(${imageUrl})` }}
-				className={classes.card}>
-				<div>
-					<Text className={classes.category} size="md">
-						{category}
-					</Text>
-				</div>
-			</Paper>
+		<LazyLoad>
+			{imageError || !imageLoaded ? (
+				<Skeleton height={440} radius="md" />
+			) : (
+				<Paper
+					shadow="md"
+					p="xl"
+					radius="md"
+					sx={{ backgroundImage: `url(${imageUrl})` }}
+					className={classes.card}>
+					<div>
+						<Text className={classes.category} size="md">
+							{category}
+						</Text>
+					</div>
+				</Paper>
+			)}
 		</LazyLoad>
 	);
 };
